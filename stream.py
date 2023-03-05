68969a5796eb4ba9a0de5bf5f6ee3dccecc46d01
SK-4 <sohamk2002@gmail.com>
2023-03-06 02:02:59 +0530

    app_id

--- stream.py
+++ stream.py
@@ -19,13 +19,13 @@ load_dotenv()
 # }
 
 firebaseConfig = {
-  "apiKey": st.secrets['FIREBASE_API_KEY'],
-  "authDomain": st.secrets['FIREBASE_AUTH_DOMAIN'],
-  "projectId": st.secrets['FIREBASE_PROJECT_ID'],
-  "databaseURL": st.secrets['FIREBASE_DATABASE_URL'],
-  "storageBucket": st.secrets['FIREBASE_STORAGE_BUCKET'],
-  "messagingSenderId": st.secrets['FIREBASE_MESSAGING_SENDER_ID'],
-  "appId": st.secrets['FIREBASE_APP']
+  "apiKey": st.secrets["FIREBASE_API_KEY"],
+  "authDomain": st.secrets["FIREBASE_AUTH_DOMAIN"],
+  "projectId": st.secrets["FIREBASE_PROJECT_ID"],
+  "databaseURL": st.secrets["FIREBASE_DATABASE_URL"],
+  "storageBucket": st.secrets["FIREBASE_STORAGE_BUCKET"],
+  "messagingSenderId": st.secrets["FIREBASE_MESSAGING_SENDER_ID"],
+  "appId": st.secrets["FIREBASE_APP_ID"]
 }
 
 # Firebase Authentication
